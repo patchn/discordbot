55c1d70573b1ac4120074ffeb6f29fa5568cd7f6
patchn <71460349+patchn@users.noreply.github.com>
2023-10-17 22:46:22 -0400

    WORKS

--- bot/src/main/java/discordbot/bot/botListeners.java
+++ bot/src/main/java/discordbot/bot/botListeners.java
@@ -2,21 +2,28 @@ package discordbot.bot;
 
 import org.jetbrains.annotations.NotNull;
 
+import net.dv8tion.jda.api.entities.Guild;
+import net.dv8tion.jda.api.entities.Member;
+import net.dv8tion.jda.api.entities.Role;
+import net.dv8tion.jda.api.entities.User;
 import net.dv8tion.jda.api.events.guild.member.GuildMemberJoinEvent;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 
 public class botListeners extends ListenerAdapter{
+    // @Override
+    // public void onMessageReceived(@NotNull MessageReceivedEvent event) { 
+    //     if (!event.getAuthor().isBot()) {
+    //         String sentMessage = event.getMessage().getContentRaw();
+    //         event.getChannel().sendMessage("lol").queue();
+    //     }
+    // }
+    
     @Override
-    public void onMessageReceived(@NotNull MessageReceivedEvent event) { 
-        if (!event.getAuthor().isBot()) {
-            String sentMessage = event.getMessage().getContentRaw();
-            event.getChannel().sendMessage("lol").queue();
-        }
-    }
-
-    @Override
-    public void autoGiveRole(@NotNull GuildMemberJoinEvent event) {
-        
+    public void onGuildMemberJoin(@NotNull GuildMemberJoinEvent event) {
+            Role role = event.getGuild().getRoleById("275323012763484160");
+            if (role != null) {
+                event.getGuild().addRoleToMember(event.getMember(), role).queue();
+            }
     }
 }
--- bot/src/main/java/discordbot/bot/discordBot.java
+++ bot/src/main/java/discordbot/bot/discordBot.java
@@ -3,12 +3,14 @@ package discordbot.bot;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.entities.Activity;
+import net.dv8tion.jda.api.requests.GatewayIntent;
 
 public class discordBot {
     public static void main(String[] args) {
         
         JDA bot = JDABuilder.createDefault("MTE2MzY1NzAyNTgyMjE0MjU4Nw.Gqs1_E.1rlVgWFYzDKBpVMZd1JQRU8VeGtvj9VcLS4GU4")
         .setActivity(Activity.playing("test"))
+        .enableIntents(GatewayIntent.GUILD_MEMBERS)
         .addEventListeners(new botListeners())
         .build();
     }
